e4d94528ec9c400ee72d5664d22928bb691babaa
Srikanth Padakanti <srikanth29.9@gmail.com>
2018-01-11 15:19:44 -0500

    Update TheaterSeat.java

--- theaterseat/TheaterSeat.java
+++ theaterseat/TheaterSeat.java
@@ -26,7 +26,22 @@ public class TheaterSeat {
         StringBuilder ticketRequests = new StringBuilder();
         boolean isLayoutFinished = false;
         
-        System.out.println("Please enter TheaterLayout and TicketRequests and then enter 'end'.\n");
+        /* 6 6 
+           3 5 5 3 
+           4 6 6 4 
+           2 8 8 2 
+           6 6
+
+           Smith 2 
+           Jones 5 
+           Davis 6 
+           Wilson 100 
+           Johnson 3 
+           Williams 4 
+           Brown 8 
+           Miller 12 
+           end */
+        System.out.println("Please enter TheaterLayout, TicketRequests seperated by an empty line and then enter 'end'.\n");
         
         Scanner input = new Scanner(System.in);
 
